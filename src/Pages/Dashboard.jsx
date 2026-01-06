f8a1ac85e419b4e1728a51f79ba26781d07d76a7
christina <christinakimario8@gmail.com>
2026-01-06 23:29:50 +0300

    staging changes to pull new commits

--- src/Pages/Dashboard.jsx
+++ src/Pages/Dashboard.jsx
@@ -336,7 +336,7 @@ const fetchAirData = async () => {
             >
               View All Sensors
             </button>
-          )}
+          )} 
         </div>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
           {sensors.map((sensor) => {
